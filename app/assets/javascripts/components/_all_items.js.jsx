240c10a9eeab48a5f388109433912d8eafd9e382
APilarca <angelpill15@gmail.com>
2017-01-15 18:55:58 -0800

    add items component

--- app/assets/javascripts/components/_all_items.js.jsx
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -1,15 +1,19 @@
 var AllItems = React.createClass({
-	handleDelete() {
-		this.props.handleDelete(id);
+	handleEdit() {
+
+	}
+
+	handleDelete(id) {
+    this.props.handleDelete(id);
 	},
 
 	render() {
 	    var items= this.props.items.map((item) => {
 	        return (
-	            <div>
-	                <h3>{item.name}</h3>
-	                <p>{item.description}</p>
-	                <button onClick={this.handleDelete}>Delete</button>
+	            <div key={item.id}>
+	                <Item item={item}
+	                	handleDelete={this.handleDelete.bind(this, item.id)}
+	                	handleEdit={this.handleEdit}/>
 	            </div>
 	        )
 	    });
--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -12,9 +12,23 @@ var Body = React.createClass({
 		this.setState({ items: newState })
 	},
 
-	handleDelete() {
-		console.log('delete item clicked');
-	}
+	handleDelete(id) {
+    $.ajax({
+        url: `/api/v1/items/${id}`,
+        type: 'DELETE',
+        success:() => {
+           this.removeItemClient(id);
+        }
+    });
+},
+
+removeItemClient(id) {
+    var newItems = this.state.items.filter((item) => {
+        return item.id != id;
+    });
+
+    this.setState({ items: newItems });
+},
 
 	render() {
 		return (
